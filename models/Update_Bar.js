bed71892896402ac110dfdb8dc578203b634a9ba
heath home pc <heath.blandford@gmail.com>
2020-03-02 21:49:26 -0500

    update update bar controller

--- models/Update_Bar.js
+++ models/Update_Bar.js
@@ -1,39 +1,43 @@
 /****************************************************
  * Fowllowing the MVC (model-view-controller) model this is the "Model"
  * this defines what the database will receive/respond with
- * 
+ *
  * The collection part of this (the end) is the name of the collection that
  * is in the DB. So in the case of this mongoDB cloud instance, 'disappointmentList'
  * is the name of the collection inside the database
- * 
+ *
  */
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const update_bar_schema = new Schema({
-  Bar             : { type: String },
-  Description     : { type: String },
-  Neighborhood    : { type: String }, 
-  Address         : { type: String },
-  Phone           : { type: String },
-  Website         : { type: String },
-  hoursMonday     : { type: String },
-  hoursTuesday    : { type: String },
-  hoursWednesday  : { type: String },
-  hoursThursday   : { type: String },
-  hoursFriday     : { type: String },
-  hoursSaturday   : { type: String },
-  hoursSunday     : { type: String },
-  specialMonday   : { type: String },
-  specialTuesday  : { type: String },
-  specialWednesday: { type: String },
-  specialThursday : { type: String },
-  specialFriday   : { type: String },
-  specialSaturday : { type: String },
-  specialSunday   : { type: String },
-  updated_on      : { type: Date, default: Date.now}
-}, {collection: 'cincy-bars-updated02', timestamps: true});
+const update_bar_schema = new Schema(
+    {
+        Bar: {
+            type: String,
+            required: true
+        },
+
+        toUpdate: {
+            type: String,
+            required: true
+        },
+
+        updateValue: {
+            type: String,
+            required: true
+        },
+
+        createdOn: {
+            type: Date,
+            default: Date.now
+        }
+    },
+    { collection: "cincy-bars-updated02", timestamps: true }
+);
 
 // * [database_name].[collection] -- test is the database name
-module.exports = mongoose.model("cincy-bars.cincy-bars-updated02", update_bar_schema);
+module.exports = mongoose.model(
+    "cincy-bars.cincy-bars-updated02",
+    update_bar_schema
+);
